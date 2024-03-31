1762130a2938f3342c2ecf80a8d008a5611a23ce
Majuniemi <149418950+Majuniemi@users.noreply.github.com>
2024-03-31 19:43:12 +0300

    GroupRouteen lisätty tietokannan SSL-true

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -11,6 +11,7 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: process.env.DB_SSL,
 });
 
 // GET-endpoint hakee kaikki tietueet taulusta Group
