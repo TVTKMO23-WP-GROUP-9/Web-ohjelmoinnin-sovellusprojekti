bb08aa29e719c02ca1a1f2d050cc9fe47de92e3e
HannuKarjalainen <143908228+HannuKarjalainen@users.noreply.github.com>
2024-04-01 09:41:46 +0300

    paranneltu comingsoon elementtiä

--- frontend/src/components/content/homepage/Comingsoon.jsx
+++ frontend/src/components/content/homepage/Comingsoon.jsx
@@ -7,6 +7,7 @@ const Comingsoon = () => {
   const [hoveredEventIndex, setHoveredEventIndex] = useState(-1);
   const [currentEventIndex, setCurrentEventIndex] = useState(0);
   const [visibleEvents, setVisibleEvents] = useState([]);
+  const [intervalId, setIntervalId] = useState(null); // Lisätty intervalId
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -40,11 +41,30 @@ const Comingsoon = () => {
   }, [currentEventIndex, events]);
 
   const handleNext = () => {
-    setCurrentEventIndex(prevIndex => Math.min(prevIndex + 1, events.length - 5));
+    if (hoveredEventIndex === -1) {
+      setCurrentEventIndex(prevIndex => Math.min(prevIndex + 1, events.length - 5));
+    }
   };
 
   const handlePrev = () => {
-    setCurrentEventIndex(prevIndex => Math.max(prevIndex - 1, 0));
+    if (hoveredEventIndex === -1) {
+      setCurrentEventIndex(prevIndex => Math.max(prevIndex - 1, 0));
+    }
+  };
+
+  const handleMouseEnter = (handler) => {
+    handler(); // Kutsu tapahtumakäsittelijää heti
+
+    // Kutsu tapahtumakäsittelijää uudelleen 0.2 sekunnin välein
+    const id = setInterval(handler, 200);
+    setIntervalId(id); // Tallenna intervalin id
+
+    // Pysäytä interval, kun hiiri poistuu nuolen päältä
+    return () => clearInterval(id);
+  };
+
+  const handleMouseLeave = () => {
+    clearInterval(intervalId); // Pysäytä interval, kun hiiri poistuu nuolen päältä
   };
 
   return (
@@ -53,7 +73,13 @@ const Comingsoon = () => {
         <p>Hetkonen...</p>
       ) : (
         <div className="events-container">
-          <img src="../src/components/content/images/leftarrow.jpg" alt="Left Arrow" onClick={handlePrev} />
+          <img
+            src="../src/components/content/images/leftarrow.jpg"
+            alt="Left Arrow"
+            onClick={handlePrev}
+            onMouseEnter={() => handleMouseEnter(handlePrev)}
+            onMouseLeave={handleMouseLeave} // Kutsu handleMouseLeave, kun hiiri poistuu
+          />
           {visibleEvents.map((event, index) => (
             <div
               key={event.id}
@@ -67,7 +93,13 @@ const Comingsoon = () => {
               </a>
             </div>
           ))}
-          <img src="../src/components/content/images/rightarrow.jpg" alt="Right Arrow" onClick={handleNext} />
+          <img
+            src="../src/components/content/images/rightarrow.jpg"
+            alt="Right Arrow"
+            onClick={handleNext}
+            onMouseEnter={() => handleMouseEnter(handleNext)}
+            onMouseLeave={handleMouseLeave} // Kutsu handleMouseLeave, kun hiiri poistuu
+          />
         </div>
       )}
     </div>
