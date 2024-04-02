e79ddea17093168c5fdda633079738cb28cd3e62
xolooh <tapsukk@hotmail.fi>
2024-04-02 19:30:35 +0300

    jostain konflikteja

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -3,17 +3,10 @@ import { Link } from 'react-router-dom';
 import LoginDropdown from './Login';
 
 const Header = ({ user, handleLogout }) => {
-<<<<<<< HEAD
   const [isburgerOpen, setIsburgerOpen] = useState(false);
 
   const toggleburger = () => {
     setIsburgerOpen(!isburgerOpen);
-=======
-  const [showMenu, setShowMenu] = useState(false);
-
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
->>>>>>> fe1bf6988e83d3988dc0d35d059ab605a6edf960
   };
 
   return (
@@ -21,7 +14,6 @@ const Header = ({ user, handleLogout }) => {
       <div id="header-bar">
         <div className="header-container">
           <h1>Fancy Title Here</h1>
-<<<<<<< HEAD
           <button className="mobile-menu-icon" onClick={toggleburger}>☰</button>
         
       <div className={`menu-items ${isburgerOpen ? 'open' : ''}`}>
@@ -37,28 +29,6 @@ const Header = ({ user, handleLogout }) => {
     </div>
     </div>
     </div>
-=======
-          </div>
-          <button className="hamburger-icon" onClick={toggleMenu}>
-            ☰
-          </button>
-          <div className="menu-items">
-            <ul>
-              <li ><Link to="/">Etusivu</Link></li>
-              <li><Link to="/search">Leffahaku</Link></li>
-              <li><Link to="/community">Yhteisö</Link></li>
-              {!user && <li style={{ marginLeft: 'auto' }}><Link to="/login">Kirjaudu</Link></li>}
-              {!user && <li><Link to="/community">Rekisteröidy</Link></li>}
-              {user && <li><Link to="/myaccount">Oma tili</Link></li>}
-              {user && <li style={{ marginLeft: 'auto' }}><Link onClick={handleLogout}>Kirjaudu ulos</Link></li>}
-              
-            </ul>
-            
-       </div>
-          </div>
-      </div>
-    
->>>>>>> fe1bf6988e83d3988dc0d35d059ab605a6edf960
     
   );
 };
