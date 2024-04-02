e2c53a5268e0c0c58605780f34f2da85fadbe98c
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2024-04-02 11:04:21 +0300

    login ja register toimii

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,13 @@ const app = express();
 
 const profile = require('./routes/profileRoutes'); // Tuo 'profile' reitityksen
 const group = require('./routes/groupRoutes'); // Tuo 'group' reitityksen
+const register = require('./routes/authRoutes'); // Tuo 'register' reitityksen
+const login = require('./routes/authRoutes'); // Tuo 'login' reitityksen
 
 app.use('/', profile);  // Käytä 'profile' reititystä juuressa
 app.use('/', group);  // Käytä 'group' reititystä juuressa
+app.use('/', register); // Käytä 'register' reititystä juuressa
+app.use('/', login); // Käytä 'login' reititystä juuressa
 
 const PORT = process.env.PORT || 3001; // Määritä portti
 
