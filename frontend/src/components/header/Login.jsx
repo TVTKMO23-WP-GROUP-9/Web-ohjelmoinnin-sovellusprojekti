e70932a28dfa03a8b7b03f4b024f6128a88aafed
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2024-04-04 16:10:43 +0300

    login pohjaa

--- frontend/src/components/header/Login.jsx
+++ frontend/src/components/header/Login.jsx
@@ -1,27 +1,47 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-const LoginDropdown = () => {
-  const [showMenu, setShowMenu] = useState(false);
 
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
+export default function Login({ setUser }) { 
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3001/auth/login', {
+        username: username,
+        password: password
+      });
+  
+      if (response.status === 200) {
+        setUser({ user: username, password: password });
+        console.log(username, password, setUsername);
+        navigate('/myaccount');
+  
+        //setShowLogin(!showLogin);
+      }
+    } catch (error) {
+      console.error('Kirjautumisvirhe:', error);
+    }
   };
+  
   return (
-    <div className="login-dropdown-container">
-      <button className="login-dropdown-button loginbutton" onClick={toggleMenu}>Kirjaudu</button>
-      {showMenu && (
+
         <div className="login-menu">
-          <form>
-            <input type="text" placeholder="Käyttäjänimi" />
-            <input type="password" placeholder="Salasana" />
-            <li><a href="/forgotPW">Unohtuiko salasana?</a></li>
-            <li><a href="/register">Rekisteröidy</a></li>
-            <button type="submit">Kirjaudu sisään</button>
+          <form onSubmit={handleLogin}>
+            <input className="field" value={username} onChange={e => setUsername(e.target.value)} placeholder="Käyttäjänimi"></input>
+            <input className="field" type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Salasana"></input>
+            <button className="formButton" type="submit">Kirjaudu sisään</button>
+            <hr/>
+            <li className="boxLink"><a href="/forgotPW">Unohtuiko salasana?</a></li>
+            <li className="boxLink"><a href="/register">Rekisteröidy</a></li>
+            
           </form>
         </div>
-      )}
-    </div>
+
   );
 };
-
-export default LoginDropdown;
