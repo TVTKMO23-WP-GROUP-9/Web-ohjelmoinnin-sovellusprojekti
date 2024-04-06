31808b5cb07f950327ba6ec2e1535b1be355c9ac
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2024-04-06 08:20:15 +0300

    responsiivisuus takas

--- frontend/src/components/content/movies/MovieDetails.jsx
+++ frontend/src/components/content/movies/MovieDetails.jsx
@@ -39,107 +39,110 @@ const MovieDetails = () => {
 
 
       return (
-        <div id="backdrop" style={movie && { backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`, backgroundSize: 'cover' }}>
+      <div id="backdrop" style={movie && { backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`, backgroundSize: 'cover' }}>
           <div className="content">
 
             {movie && (
-              <div id="backdropbg">
+            <div id="backdropbg">
     
-            <div className="moviemain">
-                  <img className="posterimg" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
+              <div className="moviemain">
+                <img className="posterimg" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
 
-                  <div className="movieinfo">
-                    <hr />
-                    <h2>{movie.title}</h2>
-                    <p><b>Kuvaus:</b> {movie.overview}</p>
-                    <p><b>Kesto:</b> {movie.runtime} min</p>
-                    <p><b>Genre:</b> {movie.genres.map(genre => genre.name).join(', ')}</p>
-                    <p><b>Julkaistu:</b> {movie.release_date}</p>
-                    <p><b>Tuotantoyhtiöt:</b> {movie.production_companies.map(company => company.name).join(', ')}</p>
-                    <p><b>Kerännyt ääniä:</b> {movie.vote_count}</p>
-                    <p><b>Äänten keskiarvo:</b> {movie.vote_average} / 10 </p>
-                  
+                <div className="movieinfo">
 
-                   {providers && providers.flatrate && providers.rent && (
-                    <table className='providers'>
-                      <tbody>
-                        <tr>
-                        <td><h3>Katso</h3></td>
-                        {providers.flatrate.map(provider => (
-                          <td key={provider.provider_id}>
-                            <a href={`https://www.themoviedb.org/movie/${movie.id}/watch`}><img src={`https://image.tmdb.org/t/p/w185${provider.logo_path}`} alt={provider.provider_name} /></a>
-                          </td>
-                        ))}
-                        </tr>
-                        <tr>
-                        <td><h3>Vuokraa</h3></td>
-                        {providers.rent.map(provider => (
+                  <h2>{movie.title}</h2>
+                  <p><b>Kuvaus:</b> {movie.overview}</p>
+                  <p><b>Kesto:</b> {movie.runtime} min</p>
+                  <p><b>Genre:</b> {movie.genres.map(genre => genre.name).join(', ')}</p>
+                  <p><b>Julkaistu:</b> {movie.release_date}</p>
+                  <p><b>Tuotantoyhtiöt:</b> {movie.production_companies.map(company => company.name).join(', ')}</p>
+                  <p><b>Kerännyt ääniä:</b> {movie.vote_count}</p>
+                  <p><b>Äänten keskiarvo:</b> {movie.vote_average} / 10 </p>
+                
+
+                  {providers && providers.flatrate && providers.rent && (
+                  <table className='providers'>
+                    <tbody>
+                      <tr>
+                      <td><h3>Katso</h3></td>
+                      {providers.flatrate.map(provider => (
                         <td key={provider.provider_id}>
                           <a href={`https://www.themoviedb.org/movie/${movie.id}/watch`}><img src={`https://image.tmdb.org/t/p/w185${provider.logo_path}`} alt={provider.provider_name} /></a>
                         </td>
-                        ))}
-                        </tr>
-                        <tr>
-                        <td colSpan="6">
-                          <a href='https://www.justwatch.com/'>Saatavuus Suomessa JustWatch</a>
-                        </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  )}
-                </div>
+                      ))}
+                      </tr>
+                      <tr>
+                      <td><h3>Vuokraa</h3></td>
+                      {providers.rent.map(provider => (
+                      <td key={provider.provider_id}>
+                        <a href={`https://www.themoviedb.org/movie/${movie.id}/watch`}><img src={`https://image.tmdb.org/t/p/w185${provider.logo_path}`} alt={provider.provider_name} /></a>
+                      </td>
+                      ))}
+                      </tr>
+                      <tr>
+                      <td colSpan="6">
+                        <a href='https://www.justwatch.com/'>Saatavuus Suomessa JustWatch</a>
+                      </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                )}
               </div>
-
-              <div className="moviereviews">
-            <h2>Viimeisimmät arvostelut</h2>
-
-              <div className="reviewslisted">
-                <b>Lähetetty:</b> 00.00.2024 <br/>
-                <b>Käyttäjältä:</b> <i>Anonymous</i> <br/>
-                <b>Arvio:</b> &#11088;&#11088;&#11088;&#11088;&#11088; [5/5] tähteä <br/>
-                <b>Perustelut:</b> <br/>
-                Esimerkkiarvostelu, tämä on placeholder vaikkapa. Ihan kiva leffa ois, jos katsoa ehtis. Kaikki aika katoaa nykyään johonkin. Epäoleelliseen? 
-                Ehkä ei kuitenkaan. Annan silti runsaat viisi tähteä, koska voin. Koska tämähän oli vain.. esimerkki yhdestä arvostelusta?<br/><br/>
-              </div>
-
-              <div className="reviewslisted">
-                <b>Lähetetty:</b> 00.00.2024 <br/>
-                <b>Käyttäjältä:</b> <i>Anonymous</i> <br/>
-                <b>Arvio:</b> &#11088;&#11088;&#11088;&#11088;&#11088; [5/5] tähteä <br/>
-                <b>Perustelut:</b> <br/>
-                Esimerkkiarvostelu, tämä on placeholder vaikkapa. Ihan kiva leffa ois, jos katsoa ehtis. Kaikki aika katoaa nykyään johonkin. Epäoleelliseen? 
-                Ehkä ei kuitenkaan. Annan silti runsaat viisi tähteä, koska voin. Koska tämähän oli vain.. esimerkki yhdestä arvostelusta?<br/><br/>
-              </div>
-
-              <div className="reviewslisted">
-                <b>Lähetetty:</b> 00.00.2024 <br/>
-                <b>Käyttäjältä:</b> <i>Anonymous</i> <br/>
-                <b>Arvio:</b> &#11088;&#11088;&#11088;&#11088;&#11088; [5/5] tähteä <br/>
-                <b>Perustelut:</b> <br/>
-                Esimerkkiarvostelu, tämä on placeholder vaikkapa. Ihan kiva leffa ois, jos katsoa ehtis. Kaikki aika katoaa nykyään johonkin. Epäoleelliseen? 
-                Ehkä ei kuitenkaan. Annan silti runsaat viisi tähteä, koska voin. Koska tämähän oli vain.. esimerkki yhdestä arvostelusta?<br/><br/>
-              </div>
-
-              <div className="reviewslisted">
-                <b>Lähetetty:</b> 00.00.2024 <br/>  
-                <b>Käyttäjältä:</b> <i>Anonymous</i> <br/>
-                <b>Arvio:</b> &#11088;&#11088;&#11088;&#11088;&#11088; [5/5] tähteä <br/>
-                <b>Perustelut:</b> <br/>
-                Esimerkkiarvostelu, tämä on placeholder vaikkapa. Ihan kiva leffa ois, jos katsoa ehtis. Kaikki aika katoaa nykyään johonkin. Epäoleelliseen? 
-                Ehkä ei kuitenkaan. Annan silti runsaat viisi tähteä, koska voin. Koska tämähän oli vain.. esimerkki yhdestä arvostelusta?<br/><br/>
-              </div>
-
-              <div className="reviewslisted">
-                <b>Lähetetty:</b> 00.00.2024 <br/>
-                <b>Käyttäjältä:</b> <i>Anonymous</i> <br/>
-                <b>Arvio:</b> &#11088;&#11088;&#11088;&#11088;&#11088; [5/5] tähteä <br/>
-                <b>Perustelut:</b> <br/>
-                Esimerkkiarvostelu, tämä on placeholder vaikkapa. Ihan kiva leffa ois, jos katsoa ehtis. Kaikki aika katoaa nykyään johonkin. Epäoleelliseen? 
-                Ehkä ei kuitenkaan. Annan silti runsaat viisi tähteä, koska voin. Koska tämähän oli vain.. esimerkki yhdestä arvostelusta?<br/><br/>
-              </div>
-
             </div>
-              
+
+            <div className="moviereviews">
+              <h2>Viimeisimmät arvostelut</h2>
+
+                <div className="reviewslisted">
+                  <b>Lähetetty:</b> 00.00.2024 <br/>
+                  <b>Käyttäjältä:</b> <i>Anonymous</i> <br/>
+                  <b>Arvio:</b> &#11088;&#11088;&#11088;&#11088;&#11088; [5/5] tähteä <br/>
+                  <b>Perustelut:</b> <br/>
+                  Esimerkkiarvostelu, tämä on placeholder vaikkapa. Ihan kiva leffa ois, jos katsoa ehtis. Kaikki aika katoaa nykyään johonkin. Epäoleelliseen? 
+                  Ehkä ei kuitenkaan. Annan silti runsaat viisi tähteä, koska voin. Koska tämähän oli vain.. esimerkki yhdestä arvostelusta?<br/><br/>
+                </div>
+
+                <div className="reviewslisted">
+                  <b>Lähetetty:</b> 00.00.2024 <br/>
+                  <b>Käyttäjältä:</b> <i>Anonymous</i> <br/>
+                  <b>Arvio:</b> &#11088;&#11088;&#11088;&#11088;&#11088; [5/5] tähteä <br/>
+                  <b>Perustelut:</b> <br/>
+                  Esimerkkiarvostelu, tämä on placeholder vaikkapa. Ihan kiva leffa ois, jos katsoa ehtis. Kaikki aika katoaa nykyään johonkin. Epäoleelliseen? 
+                  Ehkä ei kuitenkaan. Annan silti runsaat viisi tähteä, koska voin. Koska tämähän oli vain.. esimerkki yhdestä arvostelusta?<br/><br/>
+                </div>
+
+                <div className="reviewslisted">
+                  <b>Lähetetty:</b> 00.00.2024 <br/>
+                  <b>Käyttäjältä:</b> <i>Anonymous</i> <br/>
+                  <b>Arvio:</b> &#11088;&#11088;&#11088;&#11088;&#11088; [5/5] tähteä <br/>
+                  <b>Perustelut:</b> <br/>
+                  Esimerkkiarvostelu, tämä on placeholder vaikkapa. Ihan kiva leffa ois, jos katsoa ehtis. Kaikki aika katoaa nykyään johonkin. Epäoleelliseen? 
+                  Ehkä ei kuitenkaan. Annan silti runsaat viisi tähteä, koska voin. Koska tämähän oli vain.. esimerkki yhdestä arvostelusta?<br/><br/>
+                </div>
+
+                <div className="reviewslisted">
+                  <b>Lähetetty:</b> 00.00.2024 <br/>  
+                  <b>Käyttäjältä:</b> <i>Anonymous</i> <br/>
+                  <b>Arvio:</b> &#11088;&#11088;&#11088;&#11088;&#11088; [5/5] tähteä <br/>
+                  <b>Perustelut:</b> <br/>
+                  Esimerkkiarvostelu, tämä on placeholder vaikkapa. Ihan kiva leffa ois, jos katsoa ehtis. Kaikki aika katoaa nykyään johonkin. Epäoleelliseen? 
+                  Ehkä ei kuitenkaan. Annan silti runsaat viisi tähteä, koska voin. Koska tämähän oli vain.. esimerkki yhdestä arvostelusta?<br/><br/>
+                </div>
+
+                <div className="reviewslisted">
+                  <b>Lähetetty:</b> 00.00.2024 <br/>
+                  <b>Käyttäjältä:</b> <i>Anonymous</i> <br/>
+                  <b>Arvio:</b> &#11088;&#11088;&#11088;&#11088;&#11088; [5/5] tähteä <br/>
+                  <b>Perustelut:</b> <br/>
+                  Esimerkkiarvostelu, tämä on placeholder vaikkapa. Ihan kiva leffa ois, jos katsoa ehtis. Kaikki aika katoaa nykyään johonkin. Epäoleelliseen? 
+                  Ehkä ei kuitenkaan. Annan silti runsaat viisi tähteä, koska voin. Koska tämähän oli vain.. esimerkki yhdestä arvostelusta?<br/><br/>
+                </div>
+
+              </div>
+
+              <div className="justMargins">
+              <Link to="/search">Tee uusi haku</Link> 
+              </div>
 
             </div>
           )}
