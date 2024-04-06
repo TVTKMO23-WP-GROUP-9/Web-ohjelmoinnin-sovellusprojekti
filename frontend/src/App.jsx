bd82adc394c4a66ee756eb35c9cc62fbf936789c
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2024-04-06 08:24:37 +0300

    error-osa liian pienille näytöille

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import MyAccount from '@content/user/MyAccount';
 import ProfileDetails from '@content/user/ProfileDetails';
 import Search from '@content/movies/Search';
 import MovieDetails from '@content/movies/MovieDetails';
+import Error from '@content/error/Error';
 // importtaa muut sivut
 
 function App() {
@@ -57,7 +58,7 @@ function App() {
       
         <ThemeProvider>
           <div className={`body ${theme}`}>
-              
+              <Error />
               <Header user={user} setUser={handleLogin} handleLogout={handleLogout} />  
             <Routes>
               <Route path="/" exact element={<Home />} />
