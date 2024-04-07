6fcbbf3f25b4536e047f37de0e82176b30ee1834
xolooh <tapsukk@hotmail.fi>
2024-04-07 21:03:26 +0300

    viimeksi kirjautunu ei toimi. näyttää reaaliaikaa

--- backend/auth/authService.js
+++ backend/auth/authService.js
@@ -33,7 +33,7 @@ async function loginUser(username, password) {
         // salasanan tarkistus bcryptillä
         const passwordMatch = await bcrypt.compare(password, user.hashedpassword);
         if (passwordMatch) {
-            await authModel.LastLoggedIn(username);
+            await authModel.LastLoggedIn(profileid);
             return { success: true, message: 'Kirjautuminen onnistui' };
         } else {
             return { success: false, message: 'Kirjautuminen epäonnistui' };
@@ -46,5 +46,6 @@ async function loginUser(username, password) {
 
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    
 };
--- frontend/src/components/content/user/ProfileDetails.jsx
+++ frontend/src/components/content/user/ProfileDetails.jsx
@@ -7,6 +7,7 @@ import GroupList from './GroupList';
 const ProfileDetails = ({ user }) => {
     const [profile, setProfile] = useState(null);
     const { profilename } = useParams();
+    const [lastLoggedIn, setLastLoggedIn] = useState(null);
 
 useEffect(() => {
     const fetchProfile = async () => {
@@ -21,16 +22,22 @@ useEffect(() => {
     
     fetchProfile();
 }, [user, user?.username, profilename]);
+useEffect(() => {
+    const simulateLogin = async () => {
+        // Tässä voit esimerkiksi hakea kirjautumisajan palvelimelta
+        const currentTime = new Date().toLocaleString();
+        setLastLoggedIn(currentTime);
+    };
 
-  //const isOwnProfile = user && profile && profile.profilename === user.username;
+    simulateLogin();
+}, [user]);
 const isOwnProfile = user && profile && user.username && profile.profilename === user.username;
-
     return (
         <div className="content">
             <div className="inner-view">
                 <div className="inner-left">
                     <img src={profile?.profilepicurl || ''} className="profilepic" alt="Käyttäjän kuva" />
-                    <span>Viimeksi kirjautuneena: {profile?.lastLoggedIn}</span>
+                    <span>Viimeksi kirjautuneena: {lastLoggedIn}</span>
                     <br />
                     {isOwnProfile && <Link to={`/profile/${profilename}/edit`} className="basicbutton">Muokkaa profiilia</Link>}
                 </div>
