19d6748aeaa51bddabf6472964ea5ba3b116358b
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2024-04-07 16:32:07 +0300

    maksimimerkkimäärä kuvaukselle

--- frontend/src/components/content/movies/Movies.jsx
+++ frontend/src/components/content/movies/Movies.jsx
@@ -139,7 +139,7 @@ const Movies = () => {
           <img src={movie.poster_path} alt={movie.title} />
           <div className="headoverview">
             <div><h3>{movie.title}</h3></div>
-            <div>{movie.overview}</div>
+            <div>{movie.overview.length > 200 ? `${movie.overview.substring(0, 200)}...` : movie.overview}</div>
           </div>
           </Link>
         </div>
