8d4ab3f04167e36d60a8625ae2af5df0bd212da9
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2024-04-08 22:08:52 +0300

    database skeema + testidata kunnossa ja movieDetails palautettu aiempi piste

--- postgredump/skeema-testidatalla.sql
+++ postgredump/skeema-testidatalla.sql
@@ -11,7 +11,6 @@ DROP TABLE IF EXISTS Message_ CASCADE;
 DROP TABLE IF EXISTS Profile_ CASCADE;
 DROP TABLE IF EXISTS Review_ CASCADE;
 
-
 CREATE TABLE IF NOT EXISTS Profile_
 (
     profileid SERIAL PRIMARY KEY,
@@ -23,7 +22,6 @@ CREATE TABLE IF NOT EXISTS Profile_
     "timestamp" TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     description text COLLATE pg_catalog."default"
 );
-
 
 CREATE TABLE IF NOT EXISTS Group_
 (
@@ -123,7 +121,8 @@ ALTER TABLE IF EXISTS Review_
     ON UPDATE CASCADE
     ON DELETE CASCADE;
 
-INSERT INTO Profile_ (profilename, hashedPassword, email, profilepicurl, "timestamp", description) VALUES
+-- vipu, että käyttäjä saa profiilinsa yksityiseksi
+INSERT INTO Profile_ (profilename, hashedpassword, email, profilepicurl, "timestamp", description) VALUES
 -- testikäyttäjät id 1-10
 -- Viilipytty hashaamaton salasana: oivamato
 ('Viilipytty', '$2b$10$Os132x/7gfQuEkIpTNvTG.XMrnQG/bOgj4AepLQ5u.wtgfEoTFpf.', 'viilipytty@example.com', NULL, CURRENT_TIMESTAMP, NULL),
@@ -176,159 +175,6 @@ INSERT INTO Profile_ (profilename, hashedPassword, email, profilepicurl, "timest
 -- UUUseri hashaamaton salasana: 123456
 ('UUUseri', '$2b$10$09CaL5r.JxvCl2rm2F5BBudYNinrgKKqPfcuVGEXJvdJwub0Q06te', 'uuuseri@example.com', NULL, CURRENT_TIMESTAMP, NULL);
 
-
-INSERT INTO Group_ (groupname, groupexplanation, "timestamp") VALUES
--- id 1-5
-('Ryhmärämä', 'kuvaus', CURRENT_TIMESTAMP),
-('komediahullut', 'kuvaus', CURRENT_TIMESTAMP),
-('tosimiesklubi', 'kuvaus', CURRENT_TIMESTAMP),
-('Stadinrinki', 'kuvaus', CURRENT_TIMESTAMP),
-('SOTA-leffat', 'kuvaus', CURRENT_TIMESTAMP),
--- id 6-10
-('Draamailijat', 'kuvaus', CURRENT_TIMESTAMP),
-('kauhalla kauhuu', 'kuvaus', CURRENT_TIMESTAMP),
-('Tampereen seudun leffailijat', 'kuvaus', CURRENT_TIMESTAMP),
-('Leffatiistai', 'kuvaus', CURRENT_TIMESTAMP),
-('Vain-sarjoja', 'kuvaus', CURRENT_TIMESTAMP),
--- id 11-15
-('Suomalaista filmii', 'kuvaus', CURRENT_TIMESTAMP),
-('lastenleffat', 'kuvaus', CURRENT_TIMESTAMP),
-('Äksönittäret', 'kuvaus', CURRENT_TIMESTAMP),
-('Hevosaiheiset elokuvat', 'kuvaus', CURRENT_TIMESTAMP),
-('Oulun oikeamieliset', 'kuvaus', CURRENT_TIMESTAMP);
-
-INSERT INTO Memberlist_ (profileid, mainuser, groupid, pending) VALUES
-
--- Ryhmä Ryhmärämä
-('1', 1, '1', 0),
-('2', 0, '1', 0),
-('3', 0, '1', 0),
-('21', 0, '1', 0),
-('22', 0, '1', 0),
-('23', 0, '1', 0),
--- Ryhmä komediahullut
-('4', 1, '2', 0),
-('18', 0, '2', 0),
-('3', 0, '2', 0),
-('17', 0, '2', 0),
-('20', 0, '2', 0),
--- Ryhmä tosimiesklubi
-('5', 1, '3', 0),
-('6', 0, '3', 0),
-('11', 0, '3', 0),
-('12', 0, '3', 0),
-('13', 0, '3', 0),
--- Ryhmä Stadinrinki
-('9', 1, '4', 0),
-('7', 0, '4', 0),
-('2', 0, '4', 0),
-('8', 0, '4', 0),
-('4', 0, '4', 0),
--- Ryhmä SOTA-leffat
-('10', 1, '5', 0),
-('15', 1, '5', 0),
-('12', 0, '5', 0),
-('16', 0, '5', 0),
-('5', 0, '5', 0),
--- Ryhmä Draamailijat
-('10', 1, '6', 0),
-('21', 0, '6', 0),
-('3', 0, '6', 0),
--- Ryhmä kauhalla kauhuu
-('9', 1, '7', 0),
-('14', 0, '7', 0),
-('16', 0, '7', 0),
-('12', 0, '7', 0),
-('21', 0, '7', 0),
--- Ryhmä Tampereen seudun leffailijat
-('12', 0, '8', 0),
-('23', 1, '8', 0),
-('2', 0, '8', 0),
-('8', 0, '8', 0),
-('7', 0, '8', 0),
--- Ryhmä Leffatiistai
-('13', 0, '9', 0),
-('10', 1, '9', 0),
-('12', 0, '9', 0),
-('23', 0, '9', 0),
--- Ryhmä Vain-sarjoja
-('7', 1, '10', 0),
-('2', 0, '10', 0),
-('15', 0, '10', 0),
-('10', 0, '10', 0),
--- Ryhmä Suomalaista filmii
-('16', 0, '11', 0),
-('18', 1, '11', 0),
-('23', 0, '11', 0),
-('21', 0, '11', 0),
-('13', 0, '11', 0),
--- Ryhmä lastenleffat
-('14', 1, '12', 0),
-('16', 0, '12', 0),
-('21', 0, '12', 0),
-('2', 0, '12', 0),
--- Ryhmä Äksönittäret
-('15', 1, '13', 0),
-('22', 1, '13', 0),
-('23', 0, '13', 0),
-('10', 0, '13', 0),
-('8', 0, '13', 0),
--- Ryhmä Hevosaiheiset elokuvat
-('8', 1, '14', 0),
-('7', 0, '14', 0),
-('2', 0, '14', 0),
-('16', 0, '14', 0),
-('23', 0, '14', 0),
--- Ryhmä '15
-('11', 1, '15', 0),
-('10', 0, '15', 0),
-('8', 0, '15', 0),
-('18', 0, '15', 0),
-('14', 0, '15', 0);
-
-INSERT INTO Favoritelist_ (profileid, groupid, favoriteditem, showtime, "timestamp") VALUES
-('1', '1', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
-('2', '2', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
-('3', '3', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
-('4', '3', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
-('5', '4', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
-('6', '7', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
-('7', '10', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
-('8', '14', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
-('9', '4', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
-('10', '5', 'favorited_item_here', NULL, CURRENT_TIMESTAMP);
-
-INSERT INTO Message_ (groupid, profileid, message, "timestamp") VALUES
-('1', '1', 'Test message here', CURRENT_TIMESTAMP),
-('2', '2', 'Test message here', CURRENT_TIMESTAMP),
-('3', '3', 'Test message here', CURRENT_TIMESTAMP),
-('4', '4', 'Test message here', CURRENT_TIMESTAMP),
-('5', '5', 'Test message here', CURRENT_TIMESTAMP),
-('6', '6', 'Test message here', CURRENT_TIMESTAMP),
-('7', '7', 'Test message here', CURRENT_TIMESTAMP),
-('8', '8', 'Test message here', CURRENT_TIMESTAMP),
-('9', '9', 'Test message here', CURRENT_TIMESTAMP),
-('10', '10', 'Test message here', CURRENT_TIMESTAMP),
-('11', '11', 'Test message here', CURRENT_TIMESTAMP),
-('12', '12', 'Test message here', CURRENT_TIMESTAMP),
-('13', '13', 'Test message here', CURRENT_TIMESTAMP),
-('14', '14', 'Test message here', CURRENT_TIMESTAMP),
-('15', '15', 'Test message here', CURRENT_TIMESTAMP);
-
-INSERT INTO Review_ (profileid, revieweditem, review, rating, "timestamp") VALUES
-('1', 'reviewed_item_here', 'Test review', 5, CURRENT_TIMESTAMP),
-('2', 'reviewed_item_here', 'Test review', 4, CURRENT_TIMESTAMP),
-('3', 'reviewed_item_here', 'Test review', 3, CURRENT_TIMESTAMP),
-('4', 'reviewed_item_here', 'Test review', 2, CURRENT_TIMESTAMP),
-('5', 'reviewed_item_here', 'Test review', 4, CURRENT_TIMESTAMP),
-('6', 'reviewed_item_here', 'Test review', 3, CURRENT_TIMESTAMP),
-('7', 'reviewed_item_here', 'Test review', 5, CURRENT_TIMESTAMP),
-('8', 'reviewed_item_here', 'Test review', 4, CURRENT_TIMESTAMP),
-('9', 'reviewed_item_here', 'Test review', 3, CURRENT_TIMESTAMP),
-('10', 'reviewed_item_here', 'Test review', 2, CURRENT_TIMESTAMP);
-
-
--- updatella kuvaukset testikäyttäjille
 UPDATE Profile_
 SET description = 
     CASE 
@@ -452,46 +298,170 @@ UPDATE Profile_
 SET profilepicurl = 'https://i.postimg.cc/cCFbHZN9/profile7.jpg'
 WHERE profileid = '23';
 
--- vipu, että käyttäjä saa profiilinsa yksityiseksi
-ALTER TABLE Profile_
-ADD COLUMN is_private BOOLEAN DEFAULT FALSE;
-
--- yksityinen tili käyttäjille 8-14
-UPDATE Profile_
-SET is_private = TRUE
-WHERE profileid BETWEEN 8 AND 14;
-
 INSERT INTO Group_ (groupname, groupexplanation, "timestamp") VALUES
+-- id 1-5
+('Ryhmärämä', 'kuvaus', CURRENT_TIMESTAMP),
+('komediahullut', 'kuvaus', CURRENT_TIMESTAMP),
+('tosimiesklubi', 'kuvaus', CURRENT_TIMESTAMP),
+('Stadinrinki', 'kuvaus', CURRENT_TIMESTAMP),
+('SOTA-leffat', 'kuvaus', CURRENT_TIMESTAMP),
+-- id 6-10
+('Draamailijat', 'kuvaus', CURRENT_TIMESTAMP),
+('kauhalla kauhuu', 'kuvaus', CURRENT_TIMESTAMP),
+('Tampereen seudun leffailijat', 'kuvaus', CURRENT_TIMESTAMP),
+('Leffatiistai', 'kuvaus', CURRENT_TIMESTAMP),
+('Vain-sarjoja', 'kuvaus', CURRENT_TIMESTAMP),
+-- id 11-15
+('Suomalaista filmii', 'kuvaus', CURRENT_TIMESTAMP),
+('lastenleffat', 'kuvaus', CURRENT_TIMESTAMP),
+('Äksönittäret', 'kuvaus', CURRENT_TIMESTAMP),
+('Hevosaiheiset elokuvat', 'kuvaus', CURRENT_TIMESTAMP),
+('Oulun oikeamieliset', 'kuvaus', CURRENT_TIMESTAMP),
+-- id 16
 ('Kaikenkarvaiset koirat', 'tässä leffaryhmä koirien ystäville. Kaikki koirafilmit sallittuja ja erittäin toivottuja!', CURRENT_TIMESTAMP);
 
 INSERT INTO Memberlist_ (profileid, mainuser, groupid, pending) VALUES
+-- Ryhmä Ryhmärämä
+('1', 1, '1', 0),
+('2', 0, '1', 0),
+('3', 0, '1', 0),
+('21', 0, '1', 0),
+('22', 0, '1', 0),
+('23', 0, '1', 0),
+-- Ryhmä komediahullut
+('4', 1, '2', 0),
+('18', 0, '2', 0),
+('3', 0, '2', 0),
+('17', 0, '2', 0),
+('20', 0, '2', 0),
+-- Ryhmä tosimiesklubi
+('5', 1, '3', 0),
+('6', 0, '3', 0),
+('11', 0, '3', 0),
+('12', 0, '3', 0),
+('13', 0, '3', 0),
+-- Ryhmä Stadinrinki
+('9', 1, '4', 0),
+('7', 0, '4', 0),
+('2', 0, '4', 0),
+('8', 0, '4', 0),
+('4', 0, '4', 0),
+-- Ryhmä SOTA-leffat
+('10', 1, '5', 0),
+('15', 1, '5', 0),
+('12', 0, '5', 0),
+('16', 0, '5', 0),
+('5', 0, '5', 0),
+-- Ryhmä Draamailijat
+('10', 1, '6', 0),
+('21', 0, '6', 0),
+('3', 0, '6', 0),
+-- Ryhmä kauhalla kauhuu
+('9', 1, '7', 0),
+('14', 0, '7', 0),
+('16', 0, '7', 0),
+('12', 0, '7', 0),
+('21', 0, '7', 0),
+-- Ryhmä Tampereen seudun leffailijat
+('12', 0, '8', 0),
+('23', 1, '8', 0),
+('2', 0, '8', 0),
+('8', 0, '8', 0),
+('7', 0, '8', 0),
+-- Ryhmä Leffatiistai
+('13', 0, '9', 0),
+('10', 1, '9', 0),
+('12', 0, '9', 0),
+('23', 0, '9', 0),
+-- Ryhmä Vain-sarjoja
+('7', 1, '10', 0),
+('2', 0, '10', 0),
+('15', 0, '10', 0),
+('10', 0, '10', 0),
+-- Ryhmä Suomalaista filmii
+('16', 0, '11', 0),
+('18', 1, '11', 0),
+('23', 0, '11', 0),
+('21', 0, '11', 0),
+('13', 0, '11', 0),
+-- Ryhmä lastenleffat
+('14', 1, '12', 0),
+('16', 0, '12', 0),
+('21', 0, '12', 0),
+('2', 0, '12', 0),
+-- Ryhmä Äksönittäret
+('15', 1, '13', 0),
+('22', 1, '13', 0),
+('23', 0, '13', 0),
+('10', 0, '13', 0),
+('8', 0, '13', 0),
+-- Ryhmä Hevosaiheiset elokuvat
+('8', 1, '14', 0),
+('7', 0, '14', 0),
+('2', 0, '14', 0),
+('16', 0, '14', 0),
+('23', 0, '14', 0),
+-- Ryhmä '15
+('11', 1, '15', 0),
+('10', 0, '15', 0),
+('8', 0, '15', 0),
+('18', 0, '15', 0),
+('14', 0, '15', 0),
 -- Kaikenkarvaiset koirat
-('1', 1, '46', 0),
-('2', 0, '46', 0),
-('3', 0, '46', 0),
-('4', 0, '46', 0),
-('5', 0, '46', 0),
-('6', 0, '46', 0),
-('7', 1, '46', 0),
-('8', 0, '46', 0),
-('9', 0, '46', 0),
-('10', 0, '46', 0),
-('11', 0, '46', 0),
-('12', 0, '46', 0),
-('13', 1, '46', 0),
-('14', 0, '46', 0),
-('15', 0, '46', 0),
-('16', 0, '46', 0),
-('17', 0, '46', 0),
-('17', 0, '46', 0),
-('18', 0, '46', 0),
-('19', 0, '46', 0),
-('20', 0, '46', 0),
-('21', 0, '46', 0),
-('22', 0, '46', 0),
-('23', 0, '46', 0);
+('1', 1, '16', 0),
+('2', 0, '16', 0),
+('3', 0, '16', 0),
+('4', 0, '16', 0),
+('5', 0, '16', 0),
+('6', 0, '16', 0),
+('7', 1, '16', 0),
+('8', 0, '16', 0),
+('9', 0, '16', 0),
+('10', 0, '16', 0),
+('11', 0, '16', 0),
+('12', 0, '16', 0),
+('13', 1, '16', 0),
+('14', 0, '16', 0),
+('15', 0, '16', 0),
+('16', 0, '16', 0),
+('17', 0, '16', 0),
+('17', 0, '16', 0),
+('18', 0, '16', 0),
+('19', 0, '16', 0),
+('20', 0, '16', 0),
+('21', 0, '16', 0),
+('22', 0, '16', 0),
+('23', 0, '16', 0);
 
--- arvosteluista testidataa käyttäjille
+INSERT INTO Favoritelist_ (profileid, groupid, favoriteditem, showtime, "timestamp") VALUES
+('1', '1', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
+('2', '2', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
+('3', '3', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
+('4', '3', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
+('5', '4', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
+('6', '7', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
+('7', '10', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
+('8', '14', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
+('9', '4', 'favorited_item_here', NULL, CURRENT_TIMESTAMP),
+('10', '5', 'favorited_item_here', NULL, CURRENT_TIMESTAMP);
+
+INSERT INTO Message_ (groupid, profileid, message, "timestamp") VALUES
+('1', '1', 'Test message here', CURRENT_TIMESTAMP),
+('2', '2', 'Test message here', CURRENT_TIMESTAMP),
+('3', '3', 'Test message here', CURRENT_TIMESTAMP),
+('4', '4', 'Test message here', CURRENT_TIMESTAMP),
+('5', '5', 'Test message here', CURRENT_TIMESTAMP),
+('6', '6', 'Test message here', CURRENT_TIMESTAMP),
+('7', '7', 'Test message here', CURRENT_TIMESTAMP),
+('8', '8', 'Test message here', CURRENT_TIMESTAMP),
+('9', '9', 'Test message here', CURRENT_TIMESTAMP),
+('10', '10', 'Test message here', CURRENT_TIMESTAMP),
+('11', '11', 'Test message here', CURRENT_TIMESTAMP),
+('12', '12', 'Test message here', CURRENT_TIMESTAMP),
+('13', '13', 'Test message here', CURRENT_TIMESTAMP),
+('14', '14', 'Test message here', CURRENT_TIMESTAMP),
+('15', '15', 'Test message here', CURRENT_TIMESTAMP);
+
 INSERT INTO Review_ (profileid, revieweditem, review, rating, "timestamp") VALUES
 ('1', '1011985', 'Hyvä elokuva!', 4, CURRENT_TIMESTAMP),
 ('1', '373571', 'Hieno elokuva!', 5, CURRENT_TIMESTAMP),
@@ -541,3 +511,8 @@ INSERT INTO Review_ (profileid, revieweditem, review, rating, "timestamp") VALUE
 ('23', '373571', 'Paras!', 5, CURRENT_TIMESTAMP),
 ('24', '1022796', 'Ei suositeltava', 2, CURRENT_TIMESTAMP),
 ('24', '695721', 'Viihdyttävä', 4, CURRENT_TIMESTAMP);
+
+-- yksityinen tili käyttäjille 8-14
+UPDATE Profile_
+SET is_private = TRUE
+WHERE profileid BETWEEN 8 AND 14;
