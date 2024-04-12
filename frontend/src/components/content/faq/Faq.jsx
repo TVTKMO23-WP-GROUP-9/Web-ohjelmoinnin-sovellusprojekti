ab6dfbb6a1d259545aa34591b54864f295b44a18
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2024-04-12 23:33:57 +0300

    faq sivua snadisti

--- frontend/src/components/content/faq/Faq.jsx
+++ frontend/src/components/content/faq/Faq.jsx
@@ -8,19 +8,33 @@ const Faq = () => {
         <div className="content">
 
             <h1>L9 - Leffaysi</h1>
+            
+                <h2 className='faq'>Yleistä</h2>
+                <div className='justMargin'>
+    
+                    Leffaysi on Oulun ammattikorkeakoulun tietotekniikan opiskelijoiden tuotos toteutuksessa web-ohjelmoinnin sovellusprojekti (Q2/2024). 
+                    Tarkoituksena oli tehdä leffaharrastajien sovellus, joka tarjoaa käyttäjälle yhteisöllisyyttä ja kokemuksia elokuvien ja sarjojen parissa.
+                </div>
 
-            <h2>Yleistä</h2>
+                <h2 className='faq'>Mistä nimi?</h2>
+                <div className='justMargin'>
+                
+                    Nimeäminen on aina melko vaikea skene. Kun kaikki muut nimihirviöt oli makusteltu, sai leffa ja ryhmänumero yhdistyä. Siitä tuli Leffaysi. Tutummin L9.
+                </div>
 
-            <div className='justMargin'>Leffaysi on Oulun ammattikorkeakoulun tietotekniikan opiskelijoiden tuotos toteutuksessa web-ohjelmoinnin sovellusprojekti (Q2/2024). 
-            Tarkoituksena oli tehdä leffaharrastajien sovellus, joka tarjoaa käyttäjälle yhteisöllisyyttä ja kokemuksia elokuvien ja sarjojen parissa.</div>
-
-
-            <h2>Mistä nimi?</h2>
-            <p>Nimeäminen on aina melko vaikea skene. Kun kaikki muut nimihirviöt oli makusteltu, sai leffa ja ryhmänumero yhdistyä. Siitä tuli Leffaysi. Tutummin L9. </p>
-
-            <h2>Muut otsikot</h2>
-            <p>muut sisällöt</p>
-
+                <h2 className='faq'>Teknologiat</h2>
+                <div className='justMargin'>
+                    
+                    Frontend on toteutettu Reactilla ja backend Node.js:llä. Tietokantana käytetään PostgreSQL:ää. <br/>
+                    Sovellus on julkaistu ------ssa. <br/>
+                    Muuta, mitä?
+                </div>
+    
+                <h2 className='faq'>Muut otsikot</h2>
+                <div className='justMargin'>
+                    
+                    Muut sisällöt
+                </div>
 
         </div>
 
