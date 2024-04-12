9b668f57b940015f88a41de8f22a9e2030d5162b
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2024-04-12 12:17:03 +0300

    teeman vaihto local strg.

--- frontend/src/ThemeProvider.jsx
+++ frontend/src/ThemeProvider.jsx
@@ -1,12 +1,21 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState(() => {
+    // tarkistus, onko teema local storagessa ja oletusteema on light, jos ei ole
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme || 'light'; 
+  });
 
   const toggleTheme = () => {
-    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+    setTheme(prevTheme => {
+      const newTheme = prevTheme === 'light' ? 'dark' : 'light';
+      // teeman tallennus local str.
+      localStorage.setItem('theme', newTheme);
+      return newTheme;
+    });
   };
 
   return (
