7b2b1d66306149cf7e862e53d5daa176082506ae
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2024-04-14 09:42:45 +0300

    loading

--- frontend/src/components/content/community/AllReviews.jsx
+++ frontend/src/components/content/community/AllReviews.jsx
@@ -10,6 +10,7 @@ const AllReviews = ({ searchTerm, setSearchTerm }) => {
   const [reviews, setReviews] = useState([]);
   const [reviewsPerPage, setReviewsPerPage] = useState(4);
   const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchReviews();
@@ -39,9 +40,11 @@ const AllReviews = ({ searchTerm, setSearchTerm }) => {
           if (review.mediatype === 0) {
             const movieResponse = await axios.get(`${VITE_APP_BACKEND_URL}/movie/${encodeURIComponent(review.revieweditem)}`);
             responseData = movieResponse.data;
+            setLoading(false);
           } else if (review.mediatype === 1) {
             const tvResponse = await axios.get(`${VITE_APP_BACKEND_URL}/series/${encodeURIComponent(review.revieweditem)}`);
             responseData = tvResponse.data;
+            setLoading(false);
           }
           if (responseData && (responseData.title || responseData.name)) {
             return {
@@ -93,53 +96,62 @@ const AllReviews = ({ searchTerm, setSearchTerm }) => {
   return (
     <div className='allreviews'>
       <ul className="review-list">
-        <li className="userinfo">
-          Palvelussa on <b>{filteredReviews.length}</b> arvostelua ja niiden keskiarvo 
-          on <b>{filteredReviews.length > 0 && (filteredReviews.reduce((sum, review) => sum + review.rating, 0) / filteredReviews.length).toFixed(1)}</b>.<br />
-          Voit luoda uusia arvosteluja elokuvien ja sarjojen sivuilta. <br />
-
-        </li>
-
-        <ul className="pagination">
-          <li>
-            <button className="buttonnext" onClick={() => setCurrentPage(currentPage > 1 ? currentPage - 1 : 1)}>
-              ⯇
-            </button>
-            &nbsp; <span className="communityinfo">selaa</span> &nbsp;
-            <button className="buttonnext" onClick={() => setCurrentPage(currentPage < Math.ceil(filteredReviews.length / reviewsPerPage) ? currentPage + 1 : Math.ceil(filteredReviews.length / reviewsPerPage))}>
-              ⯈
-            </button>
-          </li>
-        </ul>
-
-        <hr />
-
-        {currentReviews.map((review, index) => (
-          <li className='minheightrews' key={index}>
-            {review.mediatype === 0 ? (
-            <Link to={`/movie/${review.revieweditem}`}><img className='reviewimg' src={`https://image.tmdb.org/t/p/w342${review.movie.poster_path}`} alt={review.movie.title} /></Link>
-            ) : (
-              <Link to={`/series/${review.revieweditem}`}><img className='reviewimg' src={`https://image.tmdb.org/t/p/w342${review.movie.poster_path}`} alt={review.movie.name} /></Link>
-              
-            )}
-           
-            {review.mediatype === 0 ? (
-              <Link className='reviewtitle' to={`/movie/${review.revieweditem}`}>{review.movie.title}</Link>
-            ) : (
-              <Link className='reviewtitle' to={`/series/${review.revieweditem}`}>{review.movie.name}</Link>
-            )}    
-            <br/>
-            
-            
-            <span>{renderRatingIcons(review.rating)}</span>
-            <span className='userinfo'>| <b>{review.rating}/5</b> tähteä</span> <br />
-            <span className='reviewinfo'><span className='reviewinfo'>{formatDate(review.timestamp)}</span> | <Link className="reviewitems" to={`/profile/${review.userProfile.profilename}`}>{review.userProfile.profilename}</Link></span> <br />
-            <span className='userinfo'>{review.review}</span> <br />
-          </li>
-        ))}
+        {loading ? (
+          <div className="loading-text">
+            Ladataan arvosteluja...
+          </div>
+        ) : (
+          <>
+            <li className="userinfo">
+              Palvelussa on <b>{filteredReviews.length}</b> arvostelua ja niiden keskiarvo 
+              on <b>{filteredReviews.length > 0 && (filteredReviews.reduce((sum, review) => sum + review.rating, 0) / filteredReviews.length).toFixed(1)}</b>.<br />
+              Voit luoda uusia arvosteluja elokuvien ja sarjojen sivuilta. <br />
+            </li>
+  
+            <ul className="pagination">
+              <li>
+                <button className="buttonnext" onClick={() => setCurrentPage(currentPage > 1 ? currentPage - 1 : 1)}>
+                  ⯇
+                </button>
+                &nbsp; <span className="communityinfo">selaa</span> &nbsp;
+                <button className="buttonnext" onClick={() => setCurrentPage(currentPage < Math.ceil(filteredReviews.length / reviewsPerPage) ? currentPage + 1 : Math.ceil(filteredReviews.length / reviewsPerPage))}>
+                  ⯈
+                </button>
+              </li>
+            </ul>
+  
+            <hr />
+  
+            {currentReviews.map((review, index) => (
+              <li className='minheightrews' key={index}>
+                {review.mediatype === 0 ? (
+                  <Link to={`/movie/${review.revieweditem}`}>
+                    <img className='reviewimg' src={`https://image.tmdb.org/t/p/w342${review.movie.poster_path}`} alt={review.movie.title} />
+                  </Link>
+                ) : (
+                  <Link to={`/series/${review.revieweditem}`}>
+                    <img className='reviewimg' src={`https://image.tmdb.org/t/p/w342${review.movie.poster_path}`} alt={review.movie.name} />
+                  </Link>
+                )}
+  
+                {review.mediatype === 0 ? (
+                  <Link className='reviewtitle' to={`/movie/${review.revieweditem}`}>{review.movie.title}</Link>
+                ) : (
+                  <Link className='reviewtitle' to={`/series/${review.revieweditem}`}>{review.movie.name}</Link>
+                )}    
+  
+                <br/>
+                <span>{renderRatingIcons(review.rating)}</span>
+                <span className='userinfo'>| <b>{review.rating}/5</b> tähteä</span> <br />
+                <span className='reviewinfo'><span className='reviewinfo'>{formatDate(review.timestamp)}</span> | <Link className="reviewitems" to={`/profile/${review.userProfile.profilename}`}>{review.userProfile.profilename}</Link></span> <br />
+                <span className='userinfo'>{review.review}</span> <br />
+              </li>
+            ))}
+          </>
+        )}
       </ul>
     </div>
-  );
+  )
 }
 
 export default AllReviews;
