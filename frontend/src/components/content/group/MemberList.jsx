360abd01ed738e3bddd2259175daf54f95dd9ebf
HannuKarjalainen <143908228+HannuKarjalainen@users.noreply.github.com>
2024-04-17 14:54:36 +0300

    lisätty poista ryhmästä ja siihen vahvistusnappi

--- frontend/src/components/content/group/MemberList.jsx
+++ frontend/src/components/content/group/MemberList.jsx
@@ -6,10 +6,50 @@ const { VITE_APP_BACKEND_URL } = import.meta.env;
 
 
 
-const MemberList = ({ id }) => {
+const MemberList = ({ id, user }) => {
+  const [profileId, setProfileid] = useState(null);
   const [members, setMembers] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [confirmRemove, setConfirmRemove] = useState(null);
+  const [isMember, setIsMember] = useState(false);
+  const [isMainuser, setMainuser] = useState(false);
 
   useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+          const token = sessionStorage.getItem('token');
+          const headers = {
+              'Authorization': `Bearer ${token}`,
+              'Content-Type': 'application/json'
+          };
+          
+          const response = await axios.get(`${VITE_APP_BACKEND_URL}/profile/${user.user}`);
+
+          console.log("Token from sessionStorage:", token);
+          console.log("Onko tää menty kusemaan?:", user.user);
+          console.log("Profilename from token:", user);
+          console.log("Entä kuseeko tää?:", id);
+          console.log("Response from profile:", response.data);
+
+          setProfileid(response.data.profileid);
+          
+          const groupResponse = await axios.get(`${VITE_APP_BACKEND_URL}/memberstatus/${response.data.profileid}/${id}`);
+          
+          console.log("Response from status:", groupResponse.data);
+
+          if (groupResponse.data.hasOwnProperty('pending') && groupResponse.data.pending === 0) {
+            setIsMember(true);
+          }
+          console.log("Response from setMember:", groupResponse.pending);
+          if (groupResponse.data.hasOwnProperty('mainuser') && groupResponse.data.mainuser === 1) {
+            setMainuser(true);
+          }
+          console.log("Response from profile:", groupResponse.data);
+      } catch (error) {
+          console.error('Virhe haettaessa profiilitietoja:', error);
+      }
+  };
+
     const fetchMembers = async () => {
       try {
         const response = await axios.get(`${VITE_APP_BACKEND_URL}/memberlist/group/${id}/0`);
@@ -36,16 +76,54 @@ const MemberList = ({ id }) => {
       }
     };
 
+    fetchProfile();
     fetchMembers();
-  }, [id]);
+  }, [id, user]);
+
+  const handleRemoveUser= async (profileId, id) => {
+    try {
+      const memberResponse = await axios.get(`${VITE_APP_BACKEND_URL}/memberstatus/${profileId}/${id}`);
+      console.log(memberResponse); 
+      if (memberResponse && memberResponse.data && memberResponse.data.memberlistid) {
+        try {
+          await axios.delete(`${VITE_APP_BACKEND_URL}/memberstatus/${memberResponse.data.memberlistid}`);
+          window.location.reload(); 
+        } catch (error) {
+          console.error('Virhe pyynnön poistamisessa:', error);
+        }
+      } else {
+        console.error('Jäsennumeron hakeminen epäonnistui tai memberlistid puuttuu vastauksesta.');
+      }
+    } catch (error) {
+      console.error('Virhe jäsennumeron hakemisessa:', error);
+    }
+  };
 
   return (
     <>
       <ul className="profileSections">
         {members.map((member, index) => (
-          <li key={index}><Link to={`/profile/${member.name.profilename}`}>{member.name.profilename}</Link></li>
+          <li key={index}>
+            <Link to={`/profile/${member.name.profilename}`}>{member.name.profilename}</Link>&nbsp;&nbsp;&nbsp;
+            {(isMainuser && editMode) && (
+              confirmRemove === member.name.profileid ? (
+                <>
+                  <button className="confirm" onClick={() => handleRemoveUser(member.name.profileid, id)}>
+                  &nbsp;<span className='emoji'>&times;</span> Vahvista
+                  </button>
+                  <button className="compactButton" onClick={() => setConfirmRemove(null)}>Peruuta</button>
+                </>
+              ) : (
+                <button className='remove' onClick={() => setConfirmRemove(member.name.profileid)}>
+                  &nbsp;<span className='emoji'>&times;</span>
+                </button>
+              )
+            )}
+          </li>
         ))}
       </ul>
+      {(isMainuser && !editMode) && <button onClick={() => setEditMode(true)} className="basicbutton">Hallitse</button>}
+      {(isMainuser && editMode) && <button onClick={() => setEditMode(false)} className="basicbutton">Lopeta</button>}
     </>
   );
 };
