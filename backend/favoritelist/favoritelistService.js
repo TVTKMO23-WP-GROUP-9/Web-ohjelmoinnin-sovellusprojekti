d60d7f58a8cf85ee98aeea74ed38d46ecde2a9df
xolooh <tapsukk@hotmail.fi>
2024-04-24 11:41:50 +0300

    maini päivitetty

--- backend/favoritelist/favoritelistService.js
+++ backend/favoritelist/favoritelistService.js
@@ -16,11 +16,10 @@ async function getAllFavoritelist (req, res)  {
   async function getFavoritelistByProfile(req, res) {
     
     const profileid = req.params.profileid;
-    const mediatype = req.params.mediatype;
     try {
         const query = {
-            text: `SELECT * FROM favoritelist_ WHERE profileid = $1 AND mediatype = $2`,
-            values: [profileid, mediatype],
+            text: `SELECT * FROM favoritelist_ WHERE profileid = $1`,
+            values: [profileid],
         };
         const result = await favoritelistModel.queryDatabase(query);
         if (result.length > 0) {
