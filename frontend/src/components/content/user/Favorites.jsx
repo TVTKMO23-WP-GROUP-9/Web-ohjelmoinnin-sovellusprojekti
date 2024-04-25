48494e47cfb9b781454a88b9f488c1db440c66f8
HannuKarjalainen <143908228+HannuKarjalainen@users.noreply.github.com>
2024-04-25 22:28:11 +0300

    hyvin simppeli viilausta vaativa jaettava käyttäjän suosikit sivu

--- frontend/src/components/content/user/Favorites.jsx
+++ frontend/src/components/content/user/Favorites.jsx
@@ -3,22 +3,22 @@ import './user.css';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 const { VITE_APP_BACKEND_URL } = import.meta.env;
+import { getHeaders } from '@auth/token';
 
-const FavoriteList = ({ profile }) => {
-  const isOwnProfile = profile && profile.isOwnProfile;
+const FavoriteList = ({ user }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [favoritesPerPage, setfavoritesPerPage] = useState(4);
   const [favorites, setFavorites] = useState([]);
-  const [editMode, setEditMode] = useState(false);
   const [adult, setAdult] = useState(false);
+  const headers = getHeaders();
 
   useEffect(() => {
     const fetchFavorites = async () => {
       try {
-        if (profile && profile.profileid) {
-          const response = await axios.get(`${VITE_APP_BACKEND_URL}/favoritelist/profile/${profile.profileid}`);
-          console.log(profile);
+        const aresponse = await axios.get(`${VITE_APP_BACKEND_URL}/profile/${user.user}`, { headers });
           
+          setAdult(aresponse.data.adult);
+          const response = await axios.get(`${VITE_APP_BACKEND_URL}/favoritelist/profile/${aresponse.data.profileid}`);   
           
           const favoriteData = response.data;
           const favoritesWithMovies = await Promise.all(favoriteData.map(async favorite => {
@@ -45,33 +45,15 @@ const FavoriteList = ({ profile }) => {
             }
           }));
           setFavorites(favoritesWithMovies);
-        }
+        
       } catch (error) {
         console.error('Hakuvirhe:', error);
       }
     };
-    setAdult(profile.adult);
-    console.log(profile.adult);
-    fetchFavorites();
-  }, [profile]);
-  const handleEditClick = () => {
-    console.log("onko oma profiili", isOwnProfile)
-    setEditMode(!editMode);
-  };
 
-  // Poistetaan suosikeista suosikki
-  const DeleteFavorite = async (favoriteditem) => {
-    try {
-      if (profile && profile.profileid) {
-        await axios.delete(`${VITE_APP_BACKEND_URL}/favorite/${profile.profileid}/${favoriteditem}`);
-        setFavorites(favorites.filter(favorite => favorite.favoriteditem !== favoriteditem)); 
-      } else {
-        console.error('Profiili-id puuttuu');
-      }
-    } catch (error) {
-      console.error('Ei pystytty poistamaan', error);
-    }
-  };
+    fetchFavorites();
+  }, [user]);
+
 
   const indexOfLastFavorite = currentPage * favoritesPerPage;
   const indexOfFirstFavorite = indexOfLastFavorite - favoritesPerPage;
@@ -79,14 +61,12 @@ const FavoriteList = ({ profile }) => {
 
   return (
     <>
-        <span className="userinfo">
-        jaa linkki <Link className="link-style" to={`/favorites/${profile.profilename}`}>http://localhost:5173/favorites/{profile.profilename}</Link>
-      </span>
+     <h1>{user.user} suosikkilista!</h1>
     <ul className="favorite-list">
+       
       <span className="userinfo">
         Löytyi <b>{favorites.length}</b> Suosikkia.<br />
       </span>
-
       <ul className="pagination">
         <li>
           <button className="buttonnext" onClick={() => setCurrentPage(currentPage > 1 ? currentPage - 1 : 1)}>
@@ -107,17 +87,13 @@ const FavoriteList = ({ profile }) => {
             {favorite.mediatype === 0 ? (
            <div className="favorite-poster">
            <img className='favoriteimg' src={`https://image.tmdb.org/t/p/w342${favorite.movie.poster_path}`} alt={favorite.movie.title} />
-           {isOwnProfile && editMode && ( 
-                <button className="favoriteDButton" onClick={() => DeleteFavorite(favorite.favoriteditem)}>X</button> 
-              )}
+
          </div>
        ) : (
           <div className="favorite-poster">
           <img className='favoriteimg' src={`https://image.tmdb.org/t/p/w342${favorite.movie.poster_path}`} alt={favorite.movie.name} />
-          {isOwnProfile && editMode && (
-                <button className="favoriteDButton" onClick={() => DeleteFavorite(favorite.favoriteditem)}>X</button> 
-              )}
-  </div>
+
+        </div>
 
           )}
             {favorite.mediatype === 0 ? (
@@ -131,9 +107,7 @@ const FavoriteList = ({ profile }) => {
         ))}
 
      </ul>
-     <button className="compactButton" onClick={handleEditClick}>
-        {editMode ? 'Lopeta muokkaus' : 'Muokkaa suosikkeja'}
-      </button>
+
     </>
   );
 }
