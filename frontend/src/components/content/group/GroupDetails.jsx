a865ecea72bdfd8bfdd7c532e05280fcefebd64c
Majuniemi <149418950+Majuniemi@users.noreply.github.com>
2024-04-26 03:09:54 +0300

    Ryhmän näytösaikojen lista haetaa nyt ryhmän sivulla

--- frontend/src/components/content/group/GroupDetails.jsx
+++ frontend/src/components/content/group/GroupDetails.jsx
@@ -7,6 +7,7 @@ import ReviewList from './ReviewList';
 import Forum from './Forum';
 import GroupEdit from './GroupEdit';
 import FavoriteList from './FavoriteList';
+import GroupEvent from './GroupEvent';
 import { getHeaders } from '@auth/token';
 const { VITE_APP_BACKEND_URL } = import.meta.env;
 
@@ -30,15 +31,15 @@ const GroupDetails = ({ user }) => {
   useEffect(() => {
     if (user !== null && user !== undefined) {
       setAdmin(user.usertype === 'admin');
-  
+
       const fetchPending = async () => {
         try {
           const groupResponse = await axios.get(`${VITE_APP_BACKEND_URL}/memberstatus/${user.profileid}/${id}`, { headers });
-  
+
           if (groupResponse.data.hasOwnProperty('pending') && groupResponse.data.pending === 0) {
             setIsMember(true);
           }
-          if (groupResponse.data.hasOwnProperty('pending') && (groupResponse.data.pending === 1 || groupResponse.data.pending === 2 )) {
+          if (groupResponse.data.hasOwnProperty('pending') && (groupResponse.data.pending === 1 || groupResponse.data.pending === 2)) {
             setIsPending(true);
           }
           if (groupResponse.data.hasOwnProperty('mainuser') && groupResponse.data.mainuser === 1) {
@@ -50,7 +51,7 @@ const GroupDetails = ({ user }) => {
           console.error('Virhe haettaessa profiilitietoja:', error);
         }
       };
-  
+
       fetchPending();
     }
   }, [user]);
@@ -86,8 +87,8 @@ const GroupDetails = ({ user }) => {
 
   const handleApplicationToJoin = async (profileId, id) => {
     try {
-      await axios.post(`${VITE_APP_BACKEND_URL}/memberstatus/${profileId}/0/${id}/1`, {}, {headers});
-      window.location.reload(); 
+      await axios.post(`${VITE_APP_BACKEND_URL}/memberstatus/${profileId}/0/${id}/1`, {}, { headers });
+      window.location.reload();
     } catch (error) {
       console.error('Virhe pyynnön lähettämisessä:', error);
     }
@@ -96,25 +97,25 @@ const GroupDetails = ({ user }) => {
 
   const handleRemoveApplication = async (profileId, id) => {
     try {
-      if (groupMembers.length === 1) { 
+      if (groupMembers.length === 1) {
         alert('Et voi poistua ryhmästä, koska olet ainoa jäsen. Voit poistaa koko ryhmän.');
       } else {
-        if (isMainuser && groupMembers.filter(member => member.mainuser === 1).length === 1) { 
+        if (isMainuser && groupMembers.filter(member => member.mainuser === 1).length === 1) {
           alert('Et voi poistua ryhmästä, koska olet ainoa pääkäyttäjä ja ryhmä jäisi ilman ylläpitäjää. Ylennä ensin toinen jäsen pääkäyttäjäksi.');
         } else {
-      const memberResponse = await axios.get(`${VITE_APP_BACKEND_URL}/memberstatus/${profileId}/${id}`);
-      if (memberResponse && memberResponse.data && memberResponse.data.memberlistid) {
-        try {
-          await axios.delete(`${VITE_APP_BACKEND_URL}/memberstatus/${memberResponse.data.memberlistid}`, {headers});
-          window.location.reload(); 
-        } catch (error) {
-          console.error('Virhe pyynnön poistamisessa:', error);
+          const memberResponse = await axios.get(`${VITE_APP_BACKEND_URL}/memberstatus/${profileId}/${id}`);
+          if (memberResponse && memberResponse.data && memberResponse.data.memberlistid) {
+            try {
+              await axios.delete(`${VITE_APP_BACKEND_URL}/memberstatus/${memberResponse.data.memberlistid}`, { headers });
+              window.location.reload();
+            } catch (error) {
+              console.error('Virhe pyynnön poistamisessa:', error);
+            }
+          } else {
+            console.error('Jäsennumeron hakeminen epäonnistui tai memberlistid puuttuu vastauksesta.');
+          }
         }
-      } else {
-        console.error('Jäsennumeron hakeminen epäonnistui tai memberlistid puuttuu vastauksesta.');
       }
-    }
-    }
     } catch (error) {
       console.error('Virhe jäsennumeron hakemisessa:', error);
     }
@@ -139,134 +140,102 @@ const GroupDetails = ({ user }) => {
   }
 
   return (
-    <div className="content"> 
-    {loading ? (
-      <div>Ladataan sisältöä</div>
-    ) : (
-      <>
+    <div className="content">
+      {loading ? (
+        <div>Ladataan sisältöä</div>
+      ) : (
+        <>
 
-      {editMode && <GroupEdit id={id} />}
-        <div className="groupProfileMain">
-          <div className="ginner-view">
+          {editMode && <GroupEdit id={id} />}
+          <div className="groupProfileMain">
+            <div className="ginner-view">
 
-            <div className="ginner-left">
-              <img 
-                  src={group?.grouppicurl ? group.grouppicurl : '/pic.png'} 
-                  className="profilepic" 
-                  alt="Käyttäjän kuva" 
+              <div className="ginner-left">
+                <img
+                  src={group?.grouppicurl ? group.grouppicurl : '/pic.png'}
+                  className="profilepic"
+                  alt="Käyttäjän kuva"
                 />
 
-              {(isAdmin || isMainuser && !editMode) && <button onClick={() => setEditMode(true)} className="basicbutton">Muokkaa ryhmää</button>}
-              {(!isMember && !isPending && user && user.user !== null && user.user !== undefined) && (
-              <button className="basicbutton" onClick={() => handleApplicationToJoin(profileId, id)}>Liittymispyyntö</button>
-              )}
-              {isPending && (
-              <button className="basicbutton" onClick={() => handleRemoveApplication(profileId, id)}>Peru Pyyntö</button>
-              )}
-              <br />
-
-            </div>
-              
-            <div className="ginner-right">
-              <h2 id="groupname">{group?.groupname}</h2>
-              <ul>
-                <p className="info">{group?.groupexplanation || ''} </p>
-              </ul>
-
-              {(isAdmin || isMember) && (
-              <><h2>Näytä lisää</h2>
-              <div className="toggleLinks">
-                <h3 onClick={toggleMembers}><span className='emoji uni07'></span>&nbsp; Jäsenlista </h3>
-                <h3 onClick={toggleFavorites}><span className='emoji uni10'></span> Suosikit &nbsp;</h3>
-                <h3 onClick={toggleEvents}><span className='emoji uni15'></span> &nbsp; Näytösajat</h3>
-              </div></>
-            )}
-            </div>
-          </div>
-        
-          {(isAdmin || isMember) && (
-          <div className='group-between'>
-            <div className="group-view">
-              <div className='group-content'>
-                {showMembers && <MemberList id={id} user={user} />}
-                {showFavorites && <FavoriteList id={id} user={user} />}
-
-                {showEvents && (
-                
-                  <>  
-                    <span className='singleEvent'><b> Selaa / Hallinnoi / yms </b></span>
-                
-                    <span className='singleEvent'>
-                      00.00.2024 &nbsp;&nbsp;
-                      Paikkakunta &nbsp;&nbsp;
-                      Teatteri, sali X &nbsp;&nbsp;
-                      klo 00:00 &nbsp;&nbsp;
-                      <a href="#"><b>Elokuvan nimi</b></a> &nbsp;&nbsp;
-                      Lisätietoja
-                    </span>
-
-                    <span className='singleEvent'>
-                      00.00.2024 &nbsp;&nbsp;
-                      Paikkakunta &nbsp;&nbsp;
-                      Teatteri, sali X &nbsp;&nbsp;
-                      klo 00:00 &nbsp;&nbsp;
-                      <a href="#"><b>Elokuvan nimi</b></a> &nbsp;&nbsp;
-                      Lisätietoja
-                    </span>
-
-                    <span className='singleEvent'>
-                      00.00.2024 &nbsp;&nbsp;
-                      Paikkakunta &nbsp;&nbsp;
-                      Teatteri, sali X &nbsp;&nbsp;
-                      klo 00:00 &nbsp;&nbsp;
-                      <a href="#"><b>Elokuvan nimi</b></a> &nbsp;&nbsp;
-                      Lisätietoja
-                    </span>
-                    </>
-                  )}
+                {(isAdmin || isMainuser && !editMode) && <button onClick={() => setEditMode(true)} className="basicbutton">Muokkaa ryhmää</button>}
+                {(!isMember && !isPending && user && user.user !== null && user.user !== undefined) && (
+                  <button className="basicbutton" onClick={() => handleApplicationToJoin(profileId, id)}>Liittymispyyntö</button>
+                )}
+                {isPending && (
+                  <button className="basicbutton" onClick={() => handleRemoveApplication(profileId, id)}>Peru Pyyntö</button>
+                )}
+                <br />
 
               </div>
+
+              <div className="ginner-right">
+                <h2 id="groupname">{group?.groupname}</h2>
+                <ul>
+                  <p className="info">{group?.groupexplanation || ''} </p>
+                </ul>
+
+                {(isAdmin || isMember) && (
+                  <><h2>Näytä lisää</h2>
+                    <div className="toggleLinks">
+                      <h3 onClick={toggleMembers}><span className='emoji uni07'></span>&nbsp; Jäsenlista </h3>
+                      <h3 onClick={toggleFavorites}><span className='emoji uni10'></span> Suosikit &nbsp;</h3>
+                      <h3 onClick={toggleEvents}><span className='emoji uni15'></span> &nbsp; Näytösajat</h3>
+                    </div></>
+                )}
+              </div>
             </div>
-         </div>
-        )}
-      </div>
 
-      {(isAdmin || isMember) && (
-      <div className='gmessages'>
-        <h2>Keskustelu  &nbsp;<span className='emoji uni08'></span></h2>
+            {(isAdmin || isMember) && (
+              <div className='group-between'>
+                <div className="group-view">
+                  <div className='group-content'>
+                    {showMembers && <MemberList id={id} user={user} />}
+                    {showFavorites && <FavoriteList id={id} user={user} />}
 
-        <div className='msgboard'>
-          <Forum id={id} user={user} />
-        </div>
+                    {showEvents && <GroupEvent id={id} user={user} />}
 
-      </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+
+          {(isAdmin || isMember) && (
+            <div className='gmessages'>
+              <h2>Keskustelu  &nbsp;<span className='emoji uni08'></span></h2>
+
+              <div className='msgboard'>
+                <Forum id={id} user={user} />
+              </div>
+
+            </div>
+          )}
+
+          {(isAdmin || isMember) && (
+            <div className='greviews-view'>
+              <h2>Arvostelut  &nbsp;<span className='emoji uni08'></span></h2>
+              <ReviewList user={user} id={id} />
+            </div>
+          )}
+
+          {(isMember) && (
+            <>
+              {confirmDeleteId === profileId ? (
+                <>
+                  <button className="confirm" onClick={() => handleRemoveApplication(profileId, id)}>
+                    &nbsp;<span className='emoji'>&times;</span> Vahvista
+                  </button>
+                  <button className="compactButton" onClick={() => setConfirmDeleteId(null)}>Peruuta</button>
+                </>
+              ) : (
+                <button className="compactButton" onClick={() => setConfirmDeleteId(profileId)}>
+                  &nbsp;<span className='emoji'>&times;</span> Poistu ryhmästä
+                </button>
+              )}
+            </>
+          )}
+        </>
       )}
-
-      {(isAdmin || isMember) && (
-      <div className='greviews-view'>
-        <h2>Arvostelut  &nbsp;<span className='emoji uni08'></span></h2>
-        <ReviewList user={user} id={id} />
-      </div>
-      )}
-
-      {(isMember) && (
-      <>
-        {confirmDeleteId === profileId ? (
-          <>
-          <button className="confirm" onClick={() => handleRemoveApplication(profileId, id)}>
-            &nbsp;<span className='emoji'>&times;</span> Vahvista
-          </button>
-          <button className="compactButton" onClick={() => setConfirmDeleteId(null)}>Peruuta</button>
-          </>
-          ) : (
-          <button className="compactButton" onClick={() => setConfirmDeleteId(profileId)}>
-            &nbsp;<span className='emoji'>&times;</span> Poistu ryhmästä
-          </button>
-        )}
-      </>
-      )}
-      </>
-    )}
     </div>
   );
 };
---
+++ frontend/src/components/content/group/GroupEvent.jsx
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+const { VITE_APP_BACKEND_URL } = import.meta.env;
+import { getHeaders } from '@auth/token';
+
+const GroupEvent = ({ }) => {
+    const [groupEvents, setGroupEvents] = useState([]);
+    const headers = getHeaders();
+
+    useEffect(() => {
+        const fetchGroupEvents = async () => {
+            try {
+                const response = await axios.get(`${VITE_APP_BACKEND_URL}/event`, { headers });
+                console.log('response.data', response.data);
+                setGroupEvents(response.data);
+            } catch (error) {
+                console.error('Error fetching groups:', error);
+            }
+        };
+
+        fetchGroupEvents();
+    }, []);
+
+    return (
+        <div>
+            <span className='singleEvent'><b> Selaa / Hallinnoi / yms </b></span>
+            {groupEvents.map((event, index) => (
+                <span key={index} className='singleEvent'>
+                    {event.event_info.start_time} &nbsp;&nbsp;
+                    {event.event_info.theatre}, {event.event_info.auditorium} &nbsp;&nbsp;
+                    {event.event_info.title} &nbsp;&nbsp;
+                    klo {event.time} &nbsp;&nbsp;
+                    <a href={event.event_info.showUrl} target="_blank" rel="noreferrer">Osta liput</a>
+                </span>
+            ))}
+        </div>
+    );
+};
+
+export default GroupEvent;
+
+
+
+
+<>
+    <span className='singleEvent'><b> Selaa / Hallinnoi / yms </b></span>
+
+    <span className='singleEvent'>
+        00.00.2024 &nbsp;&nbsp;
+        Paikkakunta &nbsp;&nbsp;
+        Teatteri, sali X &nbsp;&nbsp;
+        klo 00:00 &nbsp;&nbsp;
+        <a href="#"><b>Titanikki</b></a> &nbsp;&nbsp;
+        Lisätietoja
+    </span>
+
+    <span className='singleEvent'>
+        00.00.2024 &nbsp;&nbsp;
+        Paikkakunta &nbsp;&nbsp;
+        Teatteri, sali X &nbsp;&nbsp;
+        klo 00:00 &nbsp;&nbsp;
+        <a href="#"><b>Terminaattori</b></a> &nbsp;&nbsp;
+        Lisätietoja
+    </span>
+
+    <span className='singleEvent'>
+        00.00.2024 &nbsp;&nbsp;
+        Paikkakunta &nbsp;&nbsp;
+        Teatteri, sali X &nbsp;&nbsp;
+        klo 00:00 &nbsp;&nbsp;
+        <a href="#"><b>Elokuvan nimi</b></a> &nbsp;&nbsp;
+        Lisätietoja
+    </span>
+</>
