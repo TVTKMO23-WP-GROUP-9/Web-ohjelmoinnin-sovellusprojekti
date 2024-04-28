a969bdae9843ff580537064582103b5d7ed7e6d2
HannuKarjalainen <143908228+HannuKarjalainen@users.noreply.github.com>
2024-04-28 21:10:29 +0300

    lisätestejä

--- backend/test/test.js
+++ backend/test/test.js
@@ -1,5 +1,5 @@
 // test/authRoutes.test.js
-require('dotenv').config({ path: '../.env.test' });
+require('dotenv').config({ path: '../../.env' });
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server');
