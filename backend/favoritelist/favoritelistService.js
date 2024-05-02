5b1e72cc6323dd4eb422a975395849652097b82f
HannuKarjalainen <143908228+HannuKarjalainen@users.noreply.github.com>
2024-05-02 10:57:31 +0300

    Lisätty että myös muut ryhmän jäsenet voi poistaa ryhmän suosikkeja

--- backend/favoritelist/favoritelistService.js
+++ backend/favoritelist/favoritelistService.js
@@ -129,7 +129,7 @@ async function deleteFavoriteFromGroup(req, res) {
       const mainuserResult = await favoritelistModel.queryDatabase(mainuserQuery);
       const mainuser = mainuserResult.mainuser;
 
-      if (mainuser === 1 || res.locals.usertype === 'admin') {
+      if (mainuser === 1 || mainuser === 0 || res.locals.usertype === 'admin') {
           const query = {
               text: 'DELETE FROM favoritelist_ WHERE groupid = $1 AND favoriteditem = $2 AND mediatype = $3',
               values: [groupid, favoriteditem, mediatype],
